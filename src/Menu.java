01f16e91c28b6efc82382d55bad58a18b44ac87f
Kiennguyen2406 <nguyenduckien2406@gmail.com>
2024-07-05 13:53:24 +0700

    khoi tao menu

---
+++ src/Menu.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    public  static void printMenu() {
+        System.out.println("Chương trình quản lý bệnh án ");
+        System.out.println("Chọn chức năng theo số");
+        System.out.println("1,Thêm mới ");
+        System.out.println("2,Xóa");
+        System.out.println("3,Xem danh sách các bệnh án");
+        System.out.println("4,Thoát");
+        System.out.println("Chon chức năng");
+    }
+
+}
