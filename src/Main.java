d883bcc14dbe54e15ad216a76b99f83a2c0c3b28
Kiennguyen2406 <nguyenduckien2406@gmail.com>
2024-07-05 14:02:05 +0700

    Hoan thanh menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            Menu.printMenu();
 
+            int choose = Integer.parseInt(scanner.nextLine());
+            switch (choose) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.out.println("Exit");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("ko co lua chon phu hop");
+                    break;
+            }
+        }
     }
 }
